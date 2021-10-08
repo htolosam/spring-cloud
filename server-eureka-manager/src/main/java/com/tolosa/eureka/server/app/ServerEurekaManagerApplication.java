a0222bc33ff298e7f7fae73e452617e2808b6823
Ho Chi Minh Tolosa Morales <ho.tolosa@avaldigitallabs.com>
2021-10-07 20:55:40 -0500

    initial commits micros y server eureka y api gateway

---
+++ server-eureka-manager/src/main/java/com/tolosa/eureka/server/app/ServerEurekaManagerApplication.java
@@ -0,0 +1,15 @@
+package com.tolosa.eureka.server.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerEurekaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerEurekaManagerApplication.class, args);
+	}
+
+}
---
+++ server-eureka-manager/src/test/java/com/tolosa/eureka/server/app/ServerEurekaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolosa.eureka.server.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerEurekaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
