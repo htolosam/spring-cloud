e9e5fa3f73f47868df75a811114c9dfb3f9bf11f
Ho Chi Minh Tolosa Morales <ho.tolosa@avaldigitallabs.com>
2021-11-11 01:24:40 -0500

    resilience4j en application.yml

--- items-manager/src/main/java/com/tolosa/items/api/config/CircuitBreakerConfiguration.java
+++ items-manager/src/main/java/com/tolosa/items/api/config/CircuitBreakerConfiguration.java
@@ -27,8 +27,15 @@ public class CircuitBreakerConfiguration {
 					.failureRateThreshold(50) // tasa de falllos del umbral
 					.waitDurationInOpenState(Duration.ofSeconds(10L)) // tiempo de espera en el estado abierto 60 seg por defecto
 					.permittedNumberOfCallsInHalfOpenState(5) // permitir el numero de llamadas en estado semiabierto
+					//por defecto 100%, porcentaje de umbral de llamadas lentas, si el % de llamadas es mayor que el umbral
+					//se va a corto circuito
+					.slowCallRateThreshold(50)
+					//se configura el tiempo de duracion maximo que puede durar una llmada, se debe tener en cuenta el timeout
+					//porque va primero que este y es el que se lanzara si tienen la misma duracion
+					.slowCallDurationThreshold(Duration.ofSeconds(2L))
 					.build())
-					.timeLimiterConfig(TimeLimiterConfig.ofDefaults()) // time out
+					//.timeLimiterConfig(TimeLimiterConfig.ofDefaults()) // time out
+					.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(6L)).build())
 					.build();
 		});
 	}
