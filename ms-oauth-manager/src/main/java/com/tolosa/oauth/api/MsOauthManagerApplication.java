b2e4f565ddc72c0ce0eef1b4667196f66e4f2c20
Ho Chi Minh Tolosa Morales <ho.tolosa@avaldigitallabs.com>
2022-01-02 12:30:31 -0500

    Se habilita feign client y se configura el micro oauth en el micro gateway

--- ms-oauth-manager/src/main/java/com/tolosa/oauth/api/MsOauthManagerApplication.java
+++ ms-oauth-manager/src/main/java/com/tolosa/oauth/api/MsOauthManagerApplication.java
@@ -1,18 +1,35 @@
 package com.tolosa.oauth.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
+@EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
 @EntityScan({"com.tolosa.commons.app.models.entity"})
-public class MsOauthManagerApplication {
+public class MsOauthManagerApplication implements CommandLineRunner {
 
+	@Autowired 
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MsOauthManagerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String pass = "12345";
+		for (int i = 0; i < 5; i++) {
+			String result = this.passwordEncoder.encode(pass);
+			System.out.println(result);
+		}
 	}
 
 }
